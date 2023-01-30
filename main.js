04edabc4574fdac0be9b4730aa2f2d17d95089bc
Krishanu Datta <kdatta@akamai.com>
2023-01-30 16:25:50 -0500

    changes

---
+++ main.js
@@ -0,0 +1,38 @@
+
+// JS File for ic-05
+// Krishanu Datta, Zain Alam, George Doty
+// Last moditifed: 01.30.2023 
+
+// button clickec function
+function buttonClicked() {
+
+
+	// select the element
+	let buttonDiv = document.getElementById("button-div");
+
+	// increment the element HTML every time the button is clicked
+	buttonDiv.innerHTML = Number(buttonDiv.innerHTML) + 1;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
